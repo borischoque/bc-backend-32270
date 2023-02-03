6784e1cca88095969efcc5d8bd4e54427fd37615
borischoque <choqueb.boris@gmail.com>
2023-02-02 23:21:09 -0300

    [ADD] contenido parcial de pre-entrega (desafio 4). Se completo ruta de productos.Pendiente rutas para el carrito

---
+++ routes/productos.router.js
@@ -0,0 +1,90 @@
+const { Router } = require('express');
+const ProductManager = require('../manejoProductos.js');
+const { escribirArchivo, leerArchivo } = require('../funcAux.js')
+const router = Router();
+
+const pathFile = './data.txt';
+
+const data = new ProductManager(pathFile);
+
+/***********************************************/
+/* Devuelve todos los prouctos ó rango limitados productos*/
+/***********************************************/
+router.get('/', (req, res) => {
+
+    const { limit } = req.query;
+    const products = data.getProducts();
+
+    /* Si no se ingresa el parametro "limit" -> Se devuelve todos los productos */
+    if (!limit) return res.status(200).send(products)
+    /* Si existe el parametro "limit" -> Se devuelve lo siguiente... */
+    const limitedProducts = products.slice(0, parseInt(limit));
+    res.status(200).send(limitedProducts)
+})
+
+/***********************************************/
+/* Devuelve un producto */
+/***********************************************/
+router.get('/:idProd', (req, res) => {
+    const { idProd } = req.params
+    const productSelection = data.getProductById(parseInt(idProd))
+    /* Si el ID del producto no existe, devolvemos lo siguiente... */
+    if (!productSelection) return res.send('Product does not exist')
+
+    /* Si el ID existe, devolvemos los datos del producto */
+    res.status(200).send(productSelection)
+})
+
+/***********************************************/
+/* Eliminar un producto en particular */
+/***********************************************/
+router.delete('/:idProd', (req, res) => {
+    const { idProd } = req.params
+    /* Validacion de que producto a eliminar NO existe */
+    const productTest = data.getProductById(parseInt(idProd))
+    if (!productTest) return res.status(200).send('Product does not exist')
+    /* Eliminamos el producto (Existe)*/
+    data.deleteProduct(parseInt(idProd))
+    /* Respuesta de que elimino producto */
+    res.status(200).send(`-------Se eliminó un producto del archivo------->id: ${idProd}`)
+})
+
+/***********************************************/
+/* Agregar un producto */
+/***********************************************/
+router.post('/', (req, res) => {
+    /* Tomamos el nuevo producto agregar del body*/
+    let newProduct = req.body;
+    /* Agregamos el producto al archivo de datos */
+    data.addProduct(newProduct);
+    /* Respuesta */
+    res.status(201).send({
+        message: 'Producto Agregado',
+        newProduct
+    })
+
+})
+
+/***********************************************/
+/* Modificar un producto un producto */
+/***********************************************/
+router.put('/:idProd', (req, res) => {
+    const { idProd } = req.params;
+    /* Tomamos el nuevo producto agregar del body*/
+    let changedProduct = req.body;
+    /* Validacion de que producto a eliminar NO existe */
+    const productTest = data.getProductById(parseInt(idProd))
+    if (!productTest) return res.status(200).send('Product does not exist')
+    /* Actualizamos la propiedades del producto */
+    data.updateProduct(parseInt(idProd), changedProduct);
+    /* Respuesta */
+    res.status(201).send({
+        id: parseInt(idProd),
+        message: 'Producto Cambiado',
+        changedProduct
+    })
+
+})
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,50 +1,23 @@
 /***********************************************/
 /***********************************************/
-/********* DESAFIO 03 BORIS CHOQUE**************/
+/********* DESAFIO 04 BORIS CHOQUE**************/
 /***********************************************/
 /***********************************************/
 
 const express = require('express');
-const ProductManager = require('./manejoProductos.js');
-const { escribirArchivo, leerArchivo } = require('./funcAux.js')
+const productsRouter = require('./routes/productos.router');
+const cartRouter = require('./routes/carrito.router');
 const app = express();
 const PORT = 8081;
 
-const data = new ProductManager('./data.txt');
-
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-/***********************************************/
-/* Devuelve todos los prouctos ó limitados productos*/
-/***********************************************/
-app.get('/products', (req, res) => {
+app.use('/api/products', productsRouter);
 
-    const { limit } = req.query;
-
-    /* Si no se ingresa el parametro "limit" -> Se devuelve todos los productos */
-    if (!limit) return res.send(data.getProducts())
-
-    /* Leemos el archivo con los datos */
-    const listProducts = leerArchivo('./data.txt')
-    /* Si existe el parametro "limit" -> Se devuelve lo siguiente... */
-    const limitedProducts = listProducts.slice(0, parseInt(limit));
-    res.send(limitedProducts)
-})
-
-/***********************************************/
-/* Devuelve un producto en particular */
-/***********************************************/
-app.get('/products/:idProd', (req, res) => {
-    const { idProd } = req.params
-    productSelection = data.getProductById(parseInt(idProd))
-    /* Si el ID del producto no existe, devolvemos lo siguiente... */
-    if (!productSelection) return res.send('Product does not exist')
-
-    /* Si el ID existe, devolvemos los datos del producto */
-    res.send(productSelection)
-})
+app.use('/api/carts', cartRouter);
 
 
 app.listen(PORT, () => {
-    console.log(`Server running on Port http://localhost:${PORT}/products`);
+    console.log(`Server running on Port: ${PORT}`);
 })
